084bd4873615cd8eb0a5843765367d0b0e73e639
AZ <aaron@bonntech.com.au>
2025-02-25 17:42:26 +1100

    added mobile column in DB

--- src/Database/Migrations/2025-02-25-172712_create_auth_tables.php
+++ src/Database/Migrations/2025-02-25-172712_create_auth_tables.php
@@ -12,6 +12,7 @@ class CreateAuthTables extends Migration
         $this->forge->addField([
             'id'               => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
             'email'            => ['type' => 'varchar', 'constraint' => 255],
+            'mobile'           => ['type' => 'varchar', 'constraint' => 255],
             'username'         => ['type' => 'varchar', 'constraint' => 30, 'null' => true],
             'password_hash'    => ['type' => 'varchar', 'constraint' => 255],
             'reset_hash'       => ['type' => 'varchar', 'constraint' => 255, 'null' => true],
